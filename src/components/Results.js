e0e8f745510be309bd7ebf01863c72251403f613
Juan Rodriguez <juanrodlaya99@gmail.com>
2022-04-20 11:43:51 -0400

    added custom pictures for majors and other best fit majors

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,9 +2,15 @@ import React, {useEffect} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import * as Icons from '@fortawesome/free-solid-svg-icons'
 import "../css/Results.css";
+
 function Results ({assessmentState}) {
     assessmentState.getTotalFinancials();
     const test_results = assessmentState.getTotalPoints();
+    const related_image = assessmentState.getMajorRelatedPic(test_results);
+    var sorted_majors = assessmentState.getSortedMajors();
+    const second_major = sorted_majors.at(0);
+    const third_major = sorted_majors.at(1);
+    const fourth_major = sorted_majors.at(2);
     assessmentState = assessmentState.assessmentErase();
     useEffect(() => {
         window.addEventListener('beforeunload', alertUser)
@@ -19,7 +25,7 @@ function Results ({assessmentState}) {
         e.returnValue = ''
       }
       const handleEnd = () => {
-          assessmentState = assessmentState.assessmentErase();
+          assessmentState = assessmentState.assessmentErase();  
       }
     return (
         <div className='Results d-flex justify-content-center'>
@@ -27,14 +33,14 @@ function Results ({assessmentState}) {
                         <div className = "custom-col-flex align-items-center">
                             <div className = 'lesser-lead'>Your best fit major is:</div>
                                 <div className = 'results-header'>{test_results.MAX}</div> {/*This is where we would place our JS variable for major*/}
-                            <img className = 'results-image' src = 'https://i.imgur.com/d1NHftJ.jpeg' width = "300vw" height = "300vw"></img>
+                            <img className = 'results-image' src = {related_image.MAX} width = "300vw" height = "300vw"></img>
                         </div>
                         <div className = "custom-col-flex">
                             <div className = 'greater-lead m-2'>Other majors you may find interesting</div>
                             <ul className = "list-group list-group-flush custom-list-group m-3">
-                                <li className = "list-group-item">(2nd best fit major)</li> {/*Likewise, this is also where js variables would be placed*/}
-                                <li className = "list-group-item">(3rd best fit major)</li>
-                                <li className = "list-group-item">(4th best fit major)</li>
+                                <li className = "list-group-item">{second_major}</li> {/*Likewise, this is also where js variables would be placed*/}
+                                <li className = "list-group-item">{third_major}</li>
+                                <li className = "list-group-item">{fourth_major}</li>
                             </ul>
                             <div className = 'lesser-lead m-2'>We found that your interests also aligned with these majors so we listed them out for you as options</div>
                             <button className = "btn-save-results" href = "">Save Results?</button>
